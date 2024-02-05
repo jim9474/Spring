6497b25e315ea8f1052c4f2a6c43b31b423c74f2
jim9474 <jim9474@naver.com>
2024-02-05 17:57:21 +0900

    2024/02/05

---
+++ oBootJpa02/src/main/java/com/oracle/oBootJpa02/domain/Member.java
@@ -0,0 +1,52 @@
+package com.oracle.oBootJpa02.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@SequenceGenerator(
+		name = "member_seq_gen",					// 객체 Seq
+		sequenceName = "member_seq_generate",		// DB Seq
+		initialValue = 1,							// 시퀀스의 시작값
+		allocationSize = 1							// 시퀀스에서 한 번에 할당하는 범위
+		)
+@Table(name = "member2")
+public class Member {
+	@Id
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "member_seq_gen"
+			)
+	@Column(name = "member_id", precision = 10)
+	private Long id;
+	@Column(name = "user_name", length = 50)
+	private String name;
+	private Long sal;
+	
+	// 관계 설정
+	@ManyToOne
+	@JoinColumn(name = "team_id")
+	private Team team;
+	
+	// 실제Column X --> Buffer용도
+	@Transient
+	private String teamname;
+	
+	@Transient
+	private Long teamid;
+
+}
---
+++ oBootJpa02/src/main/java/com/oracle/oBootJpa02/repository/JpaMemberRepository.java
@@ -0,0 +1,80 @@
+package com.oracle.oBootJpa02.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.oracle.oBootJpa02.domain.Member;
+import com.oracle.oBootJpa02.domain.Team;
+
+import jakarta.persistence.EntityManager;
+
+public class JpaMemberRepository implements MemberRepository {
+	
+	private final EntityManager em;
+
+	public JpaMemberRepository(EntityManager em) {
+		this.em = em;
+	}
+
+	@Override
+	public Member save(Member member) {
+		// 팀 저장
+		Team team = new Team();
+		team.setName(member.getTeamname());
+		em.persist(team);
+		// 회원 저장
+		member.setTeam(team);          // 단방향 연관관계 설정, 참조 저장
+		em.persist(member);
+		Member member3 = em.find(Member.class, member.getId());
+		
+		System.out.println("JpaMemberRepository save member->"+member);
+		return member3;
+	}
+
+	@Override
+	public List<Member> findAll() {
+		List<Member> memberList = em.createQuery("select m from Member m", Member.class).getResultList();
+		System.out.println("JpaMemberRepository findAll memberList.size()->"+memberList.size());
+		return memberList;
+	}
+
+	@Override
+	public List<Member> findByNames(String searchName) {
+		String pname = searchName + '%';
+		System.out.println("JpaMemberRepository findByNames pname->"+pname);
+		List<Member> memberList = em.createQuery("select m from Member m where name Like :name", Member.class)
+									.setParameter("name", pname).getResultList();
+		System.out.println("JpaMemberRepository memberList.size()->"+memberList.size());
+		return memberList;
+	}
+
+	// Optional 객체를 사용하면 예상치 못한 NullPointerException 예외를 제공되는 메소드로 간단히 회피.
+	// 즉, 복잡한 조건문 없이도 널(null) 값으로 인해 발생하는 예외를 처리
+	@Override
+	public Optional<Member> findByMember(Long id) {
+		Member member = em.find(Member.class, id);
+		return Optional.ofNullable(member);
+	}
+
+	@Override
+	public void updateByMember(Member member) {
+		Member member3 = em.find(Member.class, member.getId());
+		if(member3 != null) {
+			// 팀 저장
+			Team team = em.find(Team.class, member.getTeamid());
+			if(team != null) {
+				team.setName(member.getTeamname());
+				em.persist(team);
+			}
+			// 회원 저장
+			member3.setTeam(team); 		// 단방향 연관관계 설정, 참조 저장
+			member3.setName(member.getName()); 		// 단방향 연관관계 설정, 참조 저장
+			em.persist(member3);
+			System.out.println("JpaMemberRepository updateByMember member->"+member);
+		} else {
+			System.out.println("JpaMemberRepository updateByMember No Exist..");
+		}
+		
+	}
+
+}
